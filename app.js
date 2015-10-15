69ec588fa04b7c143e84db8c91d38ac1aab8e0ea
muditdixit7 <muditdixit7@gmail.com>
2015-10-15 10:45:09 +0530

    Refactoring

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ eventEmitter.on('eventCreated', function() {
 })
 
 function callback() {
-	var server = app.listen(8087, function() {
+	var server = app.listen(8088, function() {
 		var host = server.address().address
 		var port = server.address().port
 		console.log("Server listening at http://%s:%s", host, port)
